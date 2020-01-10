8f4c5114525e4e726411f5e6ec170e16e3783c77
Ignacio Andrés Codina <igcodinap@gmail.com>
2020-01-10 15:18:28 -0300

    Facebook Script Advances

---
+++ fbscript.py
@@ -0,0 +1,50 @@
+import json
+import facebook
+
+def main():
+    token = {"EAALShho8E80BAN9rSjWcHCdIuffH9p4nFi7gMJBTF0o8i7FpZCvdjhZAhlXtxJzy9vmIwmB1HfVxAvTyteEkVRDtUhvupOd3TnS8xNjDoiFhsGZC9RJvVgI5RXqQ8zYJ2cUFJmrWzVlgFfILETEGgSwKznsFj1ZAqZAR4IeG653k7SBaAdIcNPkWVhDOSzLEl378hVMQfLtBkl3tJMaNaV8YTjce6NlwUQVEj1kLtmAZDZD"}
+    graph = facebook.GraphAPI(token)
+
+    fields = ['name,email, events']
+
+    ids = [10220832650024954]
+    
+    def exists(var):
+        if var:
+            return var
+        else:
+            return ""
+
+    for id in ids:
+        profile = graph.get_object(id, fields = fields)
+        """print(json.dumps(profile, indent=4))"""
+        data = json.dumps(profile, indent=4)
+        print(type(data))
+        decoded_data = json.loads(data)
+        """print(type(decoded_data))"""
+        events = decoded_data["events"]["data"]
+        eventsqty = len(events)
+        readingevent = 0
+        while readingevent < eventsqty:
+            """print(decoded_data["events"]["data"][readingevent])"""
+            event_id = exists(decoded_data["events"]["data"][readingevent].get("id"))
+            event_name = exists(decoded_data["events"]["data"][readingevent].get("name"))
+            description = exists(decoded_data["events"]["data"][readingevent].get("description"))
+            start_time = exists(decoded_data["events"]["data"][readingevent].get("start_time"))
+            end_time = exists(decoded_data["events"]["data"][readingevent].get("end_time"))
+
+
+            
+            print(event_name)
+            print(event_id)
+            print(description)
+            print(start_time)
+            print(end_time)
+            readingevent +=1
+            print("*********************")
+
+
+        
+
+if __name__ =="__main__":
+    main()
